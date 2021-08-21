6870e16f19b7fc4f77cbff00ede39b1a667eb811
Pavel Bogachev <boga4ev13@mail.ru>
2021-08-21 19:28:53 +0300

    feat(phpstorm): realize calculator and menu

--- main.php
+++ main.php
@@ -1,4 +1,36 @@
 <?php
 
-echo "First commit";
+function printMenu(){
+    echo "MENU\n";
+    echo "1. +\n";
+    echo "2. -\n";
+    echo "3. *\n";
+    echo "4. /\n";
+    echo "5. exit\n";
+    echo "Choose operation: ";
+}
 
+echo "Write 2 numbers:\n";
+echo "a = ";
+$a = readline();
+echo "b = ";
+$b = readline();
+do {
+    printMenu();
+    $menu = readline();
+    switch ($menu) {
+        case '+':
+            echo "a + b = " . ($a + $b)  . "\n\n";
+            break;
+        case '-':
+            echo "a - b = " . ($a - $b)  . "\n\n";
+            break;
+        case '*':
+            echo "a * b = " . ($a * $b)  . "\n\n";
+            break;
+        case '/':
+            echo "a / b = " . ($a / $b)  . "\n\n";
+            break;
+    }
+
+} while ($menu != 'exit');
